77576a7fde7f827ce3369489c2be6bbd2ea9fd97
Oliver Isaac <oisaac@gmail.com>
2021-04-13 16:33:51 -0500

    fixed line length linting issues

--- pulsar/provider.go
+++ pulsar/provider.go
@@ -188,12 +188,13 @@ var descriptions map[string]string
 
 func init() {
 	descriptions = map[string]string{
-		"admin_roles":                    "Admin roles to be attached to tenant",
-		"allowed_clusters":               "Tenant will be able to interact with these clusters",
-		"api_version":                    "Api Version to be used for the pulsar admin interaction",
-		"backlog_quota":                  "",
-		"dispatch_rate":                  "Data transfer rate, in and out of the Pulsar Broker",
-		"enable_duplication":             "ensures that each message produced on Pulsar topics is persisted to disk only once, even if the message is produced more than once",
+		"admin_roles":      "Admin roles to be attached to tenant",
+		"allowed_clusters": "Tenant will be able to interact with these clusters",
+		"api_version":      "Api Version to be used for the pulsar admin interaction",
+		"backlog_quota":    "",
+		"dispatch_rate":    "Data transfer rate, in and out of the Pulsar Broker",
+		"enable_duplication": `ensures that each message produced on Pulsar topics is persisted to disk 
+only once, even if the message is produced more than once`,
 		"encrypt_topics":                 "encrypt messages at the producer and decrypt at the consumer",
 		"max_consumers_per_subscription": "Max number of consumers per subscription",
 		"max_consumers_per_topic":        "Max number of consumers per topic",
@@ -201,12 +202,14 @@ func init() {
 		"namespace_list":                 "List of namespaces for a given tenant",
 		"namespace":                      "Pulsar namespaces are logical groupings of topics",
 		"persistence_policy":             "Policy for the namespace for data persistence",
-		"tenant":                         "An administrative unit for allocating capacity and enforcing an authentication/authorization scheme",
-		"tls_allow_insecure_connection":  "Boolean flag to accept untrusted TLS certificates",
-		"tls_cert_file":                  "Filepath to certificate file to use for TLS authentication",
-		"tls_key_file":                   "Filepath to key file to use for TLS authentication",
-		"tls_trust_certs_file_path":      "Path to a custom trusted TLS certificate file",
-		"token":                          "Authentication Token used to grant terraform permissions to modify Apace Pulsar Entities",
-		"web_service_url":                "Web service url is used to connect to your apache pulsar cluster",
+		"tenant": `An administrative unit for allocating capacity and enforcing an 
+authentication/authorization scheme`,
+		"tls_allow_insecure_connection": "Boolean flag to accept untrusted TLS certificates",
+		"tls_cert_file":                 "Filepath to certificate file to use for TLS authentication",
+		"tls_key_file":                  "Filepath to key file to use for TLS authentication",
+		"tls_trust_certs_file_path":     "Path to a custom trusted TLS certificate file",
+		"token": `Authentication Token used to grant terraform permissions
+to modify Apace Pulsar Entities`,
+		"web_service_url": "Web service url is used to connect to your apache pulsar cluster",
 	}
 }
